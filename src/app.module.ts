4c607e2e15aaee1c53582e0ea21c019800154417
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-26 18:07:43 +0900

    feat User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { Restaurant } from "./restaurnats/entities/restaurant.entity";
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from "./users/entities/user.entity";
 
 @Module({
   imports: [
@@ -33,9 +36,10 @@ import { Restaurant } from "./restaurnats/entities/restaurant.entity";
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [Restaurant],
+      entities: [User],
     }),
-    RestaurnatsModule,
+    UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
