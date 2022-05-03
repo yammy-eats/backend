7e884325d125b5ce6f9b80eac6d8fe910b572c01
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-03 16:59:24 +0900

    feat: unitTest를 위한 mock설정

---
+++ src/users/user.service.spec.ts
@@ -0,0 +1,62 @@
+import { UsersService } from './user.service';
+import { User } from './entities/user.entity';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Verification } from './entities/verification.entity';
+import { JwtService } from '../jwt/jwt.service';
+import { MailService } from '../mailer/mail.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  signin: jest.fn(),
+  signup: jest.fn(),
+};
+
+describe('UserService', () => {
+  let service: UsersService;
+
+  beforeAll(async () => {
+    const modules = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
+    }).compile();
+    service = modules.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('findById');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
+});
