7ee09cd76a3ff00685473e3564bcdc8234a757ff
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-11 18:06:08 +0900

    feat : mail service

---
+++ src/mailer/mail.service.spec.ts
@@ -0,0 +1,53 @@
+import { MailService } from './mail.service';
+import { Test } from '@nestjs/testing';
+import { JwtService } from '../jwt/jwt.service';
+import { ConfigService } from '@nestjs/config';
+import { MailerService } from '@nestjs-modules/mailer';
+
+const mockMailerService = {
+  sendMail: jest.fn(),
+};
+
+describe('MailService', () => {
+  let service: MailService;
+  let mailerService: MailerService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        MailService,
+        {
+          provide: MailerService,
+          useValue: mockMailerService,
+        },
+      ],
+    }).compile();
+    service = module.get<MailService>(MailService);
+    mailerService = module.get<MailerService>(MailerService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('_send', () => {
+    it('_send', async () => {
+      const result = await service._send([], '', '');
+      expect(mailerService.sendMail).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe('signin', () => {
+    it('sign in', async () => {
+      const result = await service.signin('');
+      expect(result).toEqual(true);
+    });
+  });
+
+  describe('signup', () => {
+    it('sign up', async () => {
+      const result = await service.signup('', '');
+      expect(result).toEqual(true);
+    });
+  });
+});
--- src/mailer/mail.service.ts
+++ src/mailer/mail.service.ts
@@ -17,22 +17,25 @@ export class MailService {
       template: `${templateName}`,
       context,
     });
-
     return true;
   }
 
-  async signin(to: string) {
+  async signin(to: string): Promise<boolean> {
     await this._send([to], '로그인 시도', 'signin.ejs', {
       email: to,
       datetime: new Date(),
     });
+
+    return true;
   }
 
-  async signup(to: string, code: string) {
+  async signup(to: string, code: string): Promise<boolean> {
     await this._send([to], '회원가입 완료', 'signup.ejs', {
       email: to,
       key: 'code',
       value: code,
     });
+
+    return true;
   }
 }
