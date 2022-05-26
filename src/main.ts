84c25b350c5043a870162ea2c4f31349ac529e25
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-26 10:28:52 +0900

    modify : port 변경

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 유효성 검사
-  await app.listen(3000);
+  await app.listen(4000);
 }
 
 bootstrap();
